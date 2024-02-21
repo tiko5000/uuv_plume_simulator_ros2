8130fc10766f4c59919d7f1e7e1f80b14e46469d
Till Koch <till.koch@uni-bremen.de>
2024-02-21 10:02:23 +0100

    add ROS2 Compatible Version of uuv_plume_simulator

---
+++ uuv_plume_simulator/uuv_plume_simulator/setup.py
@@ -0,0 +1,35 @@
+import os
+from glob import glob
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+package_name = 'uuv_plume_simulator'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    # packages=find_packages(exclude=['test']),
+    packages=["uuv_plume_simulator", "uuv_plume_simulator/uuv_gm_process", "uuv_plume_simulator/uuv_plume_model"],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        # (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        # (os.path.join('share', package_name, 'uuv_plume_simulator', 'uuv_gm_process'), glob('uuv_plume_simulator/uuv_gm_process/*.py')),
+        # (os.path.join('share', package_name, 'uuv_plume_simulator', 'uuv_plume_model'), glob('uuv_plume_simulator/uuv_plume_model/*.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='till',
+    maintainer_email='till@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'plume_server = uuv_plume_simulator.plume_server:main'
+        ],
+    },
+)
